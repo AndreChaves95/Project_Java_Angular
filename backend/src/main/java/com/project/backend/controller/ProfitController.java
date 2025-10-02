28835dbd2c1c8deab3c251073d1af5e24d8efde9
andre.chaves <andre.chaves@kuehne-nagel.com>
2025-10-02 17:47:20 +0100

    FT18: Add Cors to allow backend call

--- backend/src/main/java/com/project/backend/controller/ProfitController.java
+++ backend/src/main/java/com/project/backend/controller/ProfitController.java
@@ -5,6 +5,7 @@ import com.project.backend.dto.ShipmentDto;
 import com.project.backend.service.IProfitService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/profits")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProfitController {
 
     private final IProfitService profitService;
